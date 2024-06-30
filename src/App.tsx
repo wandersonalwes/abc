09b09b72639da420d8de71db76d4ffa0c2e33aeb
wandersonalwes <wandersonalwes@hotmail.com>
2024-06-30 19:19:17 -0300

    feat: add speech synthesis

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Button } from './components/ui/button'
+import { alphabet } from './data/alphabet'
+import { useSpeechSynthesis } from './hooks/use-speech-synthesis'
 
 export const App = () => {
+  const { speak } = useSpeechSynthesis()
+
   return (
     <div>
-      <p>App</p>
-
-      <Button>Teste</Button>
+      {alphabet.map((letter) => (
+        <Button key={letter} size="icon" variant="ghost" onClick={() => speak(letter)}>
+          {letter}
+        </Button>
+      ))}
     </div>
   )
 }
---
+++ src/hooks/use-speech-synthesis.ts
@@ -0,0 +1,25 @@
+export const useSpeechSynthesis = (lang: string = 'pt-BR') => {
+  const speak = (text: string) => {
+    if ('speechSynthesis' in window) {
+      const synth = window.speechSynthesis
+      const utterThis = new SpeechSynthesisUtterance(text)
+
+      utterThis.lang = lang
+      utterThis.onstart = () => {
+        console.log(`[STATING TO SPEAK]: ${text}`)
+      }
+      utterThis.onend = () => {
+        console.log(`[FINISH SPEAKING]: ${text}`)
+      }
+      utterThis.onerror = (event) => {
+        console.error('SpeechSynthesisUtterance.onerror', event)
+      }
+
+      synth.speak(utterThis)
+    } else {
+      alert('A síntese de fala não é suportada neste navegador.')
+    }
+  }
+
+  return { speak }
+}
